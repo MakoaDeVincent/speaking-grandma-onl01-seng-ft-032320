d1827875566ef4acef1aed56acb1b435e1fc170b
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-24 05:18:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,7 @@
 def speak_to_grandma(speak)
   if speak == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif speak == "#{speak}".upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
