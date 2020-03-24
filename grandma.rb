6a92385bf5827a4e35c4ab47834c1613a8559a92
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-24 04:55:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(speak)
+def speak_to_grandma(phrase)
 
-  if speak = "I LOVE YOU GRANDMA"
+  if phrase = "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN"
   end
