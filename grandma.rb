f8e2a0d87b97f15d1e2571d7280adbf2678fbe97
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-24 04:48:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,5 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(greeting)
-if "#{greeting}" == "#{greeting}".uppercase
-  puts "NO, NOT SINCE 1938!"
-elsif "#{greeting}" == "I LOVE YOU GRANDMA"
+if "#{greeting}" == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-else
-  puts "HUH?! SPEAK UP, SONNY!"
 end
